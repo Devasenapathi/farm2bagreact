c5f747644059888dc6b93b44f5b8ca45aae2c099
shine logics <shinelogics19@gmail.com>
2024-02-09 18:53:14 +0530

    changes

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://f2b.farm2bag.com/api/v1/";
-const BASE_URL = "http://192.168.1.32:8000/api/v1/";
+const BASE_URL = "https://f2b.farm2bag.com/api/v1/";
+// const BASE_URL = "http://192.168.1.32:8000/api/v1/";
 // login
 export const LOGINAPI = BASE_URL + "login/customer-login";
 export const VERIFYOTP = BASE_URL + "login/verify-otp-customer";
