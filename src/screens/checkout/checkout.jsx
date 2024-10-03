235f4a3eb463395e9700afc44ab6beb0d55fc762
shine <shinelogics19@gmail.com>
2024-10-03 20:31:51 +0530

    new build for changed the razorpay api to live

--- src/screens/checkout/checkout.jsx
+++ src/screens/checkout/checkout.jsx
@@ -325,7 +325,7 @@ const Checkout = () => {
         .then((res) => {
           if (res.status === 200) {
             var options = {
-              key: "rzp_test_ORBGItDlkez6EA",
+              key: "rzp_live_12DDyorrzl797Z",
               amount: res.data.result.amount,
               currency: "INR",
               name: "Farm2bag",
